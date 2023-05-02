f128d6dd8ed8b9aaf0d7008a9922042b91a6845b
AlecBorchardt <alecborchardt@gmail.com>
2023-05-02 15:01:17 -0700

    successfully assemble rockPaperScissors function

--- javascript.js
+++ javascript.js
@@ -1,11 +1,16 @@
-//variable to store player selection input
+        //variable to store player selection input
 let playerSelection = prompt("Rock, Paper or Scissors?","");
-console.log(playerSelection);
-console.log(typeof playerSelection);
 
-//function getComputerChoice randomly 
-//returns rock, paper, or scissors.
-let getComputerChoice = function(){
+        //converts input to lowercase
+playerSelection = playerSelection.toLowerCase();
+
+        //tests
+//console.log(playerSelection);
+//console.log(typeof playerSelection);
+
+        //function getComputerChoice randomly 
+        //returns rock, paper, or scissors.
+function getComputerChoice(){
     switch ((Math.floor(Math.random() * 3) + 1)){
     case 1:
         return "rock";
@@ -17,23 +22,52 @@ let getComputerChoice = function(){
         return "ya done goofed the code!";
 }
 }
-console.log(getComputerChoice());
-console.log(typeof getComputerChoice);
+
+        //trying to store the value of above function as variable
+        //and testing if it stays consistent.
+let computerChoice = getComputerChoice();
+//console.log(computerChoice);
+
+        //tests
+//console.log(typeof computerChoice);
+
 //function rockPaperScissors  
 //takes playerSelection and getComputerChoice and 
 //returns winner message.
 //*** make user input case insensitive
 
-function rockPaperScissors(playerSelection, getComputerChoice){
-    if ((playerSelection) == (String(getComputerChoice))){
-        console.log("It's a draw!");
+function rockPaperScissors(playerSelection, computerChoice){
+    console.log(playerSelection);
+    console.log(computerChoice);
+
+
+    if (playerSelection == "rock" && computerChoice == "scissors"){
+        console.log("rock beats scissors. you win!");
     }
-    else {
-        console.log("hey, I think it's working...");
+    else if (playerSelection == "rock" && computerChoice == "paper"){
+        console.log("paper beats rock. you lose!");
     }
+
+    else if (playerSelection == "paper" && computerChoice == "rock"){
+        console.log("paper beats rock. you win!");
+    }
+    else if (playerSelection == "paper" && computerChoice == "scissors"){
+        console.log("scissors beats paper. you lose!");
+    }
+
+    else if (playerSelection == "scissors" && computerChoice == "paper"){
+        console.log("scissors beats paper. you win!");
+    }
+    else if (playerSelection == "scissors" && computerChoice == "rock"){
+        console.log("rock beats scissors. you lose!");
+    }
+
+    else if (computerChoice == playerSelection){
+        console.log("It's a tie!");
+    }
+    
 }
-rockPaperScissors();
-//return the results of the call and also console.log to test after.
-//Math.floor(Math.random() * 3) + 1;
-//if i set rock paper and scissors to 
-//1,2,3, I can return a corresponding random. 
+
+//testing the game
+rockPaperScissors(playerSelection, computerChoice);
+
